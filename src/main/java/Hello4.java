b515ca609a15a540398688237c1c498fba965536
myJavaPRJ <ijh0547@naver.com>
2024-03-18 13:35:55 +0900

    기본

--- src/main/java/Hello4.java
+++ src/main/java/Hello4.java
@@ -1,6 +1,8 @@
 public class Hello4 {
-
     public static void main(String[] args) {
-        System.out.println("Hello Mr.my yestarday");
+
+
+
+        System.out.println("네번째 생성한 파일");
     }
 }
