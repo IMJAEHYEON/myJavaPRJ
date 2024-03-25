103b748b671257cd3e6a8772e102b290024b0a83
myJavaPRJ <ijh0547@naver.com>
2024-03-25 14:34:38 +0900

    모든 공백을 제거하는 replaceAll

---
+++ src/main/java/Code04_13_2.java
@@ -0,0 +1,12 @@
+public class Code04_13_2 {
+    public static void main(String[] args) {
+        String str = "한글   ABCD   efgh";
+        String cutStr;
+
+        cutStr = str.replaceAll(" ","");
+
+        System.out.println("기존 문자열 ==> [" + str + "]");
+        System.out.println("모든공백 제거 ==> ["+ cutStr +"]");
+
+    }
+}
