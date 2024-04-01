e9deac12f6b15838e8b545e329cfb1afe858157b
myJavaPRJ <ijh0547@naver.com>
2024-04-01 14:40:02 +0900

    4월1일 조건문 if , if ~else ,중첩 if문

---
+++ src/main/java/Code05_01_2.java
@@ -0,0 +1,16 @@
+public class Code05_01_2 {
+    public static void main(String[]args){
+
+        int num = 99;
+
+        if(num > 100)
+            System.out.println("100보다 작습니다");
+            System.out.println("난 무조건 실행된다"); 
+            // if문은 if문 밑에 한줄만을 인식하고 출력한다, 여러줄을 출력하려면 }{ 으로 포함시켜야한다
+
+        if(num < 100)
+            System.out.println("100보다 작습니다");
+
+        System.out.println("난 무조건 실행된다");
+    }
+}
