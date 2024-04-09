0b77aa2e6d335412bac4e7d38616782a1b1b8c95
myJavaPRJ <ijh0547@naver.com>
2024-04-09 15:44:40 +0900

    반복문

---
+++ src/main/java/ch6/Code06_06.java
@@ -0,0 +1,17 @@
+package ch6;
+
+public class Code06_06 {
+    public static void main(String[] args) {
+
+        int hap = 0;
+
+        for (int i = 1 ; i <= 10 ; i++){
+            //    hap = hap + i;
+            //    int hap = 0;
+            //    hap += 1;
+            hap += i;
+            System.out.println("hap : " + hap);
+        }
+        System.out.println("1부터 10까지의 합계 : "+ hap);
+    }
+}
