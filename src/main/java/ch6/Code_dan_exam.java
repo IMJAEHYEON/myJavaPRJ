adbeec04ff6a443499bf2f40b620c8b265da99cb
myJavaPRJ <ijh0547@naver.com>
2024-04-15 15:42:14 +0900

    중첩 반복문 이용해서 구구단 -2

---
+++ src/main/java/ch6/Code_dan_exam.java
@@ -0,0 +1,12 @@
+package ch6;
+
+public class Code_dan_exam {
+    public static void main(String[] args) {
+
+        for(int i = 2; i <= 9; i++){
+            for(int k = 1; k <= 9; k++) {
+                System.out.println(i + " x " + k + " = " + i * k);
+            }
+        }
+    }
+}
