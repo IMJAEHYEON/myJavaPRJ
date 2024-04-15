f2402bdbede9e6ea5ab3b12238bd247d4b94646c
myJavaPRJ <ijh0547@naver.com>
2024-04-15 15:41:56 +0900

    중첩 반복문 이용해서 구구단

---
+++ src/main/java/ch6/Code06_dan.java
@@ -0,0 +1,18 @@
+package ch6;
+
+public class Code06_dan {
+    public static void main(String[] args) {
+
+/*        for(int i = 1; i <= 9; i++){
+            System.out.println("2 x " + i + "= " + 2 * i);
+        }
+        for(int i = 1; i <= 9; i++){
+            System.out.println("3 x " + i + "= " + 3 * i);
+        }*/
+        for(int i = 2; i <= 3; i++){
+            for(int j = 1; j <= 9; j++){
+                System.out.println(i + " x " + j + " = " + i * j);
+            }
+        }
+    }
+}
