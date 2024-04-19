9e351670767f980e234c2f78d0859126e4f2745d
myJavaPRJ <ijh0547@naver.com>
2024-04-19 10:25:34 +0900

    a에서 b까지 홀수의 합 구하는 프로그램 복습시험

---
+++ src/main/java/test.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int hap = 0;
+
+        if (a < b) {
+            for (int i = a; i <= b; i++) {
+                if (i % 2 != 0) {
+                    hap += i;
+                }
+            }
+        } else if(a>b){
+            for (int i = a; i > b; i--) {
+                if (i % 2 != 0) {
+                    hap += i;
+
+                }
+            }
+        }
+        System.out.println("a 부터 b 까지의 홀수의 합 :" + hap);
+    }
+}
