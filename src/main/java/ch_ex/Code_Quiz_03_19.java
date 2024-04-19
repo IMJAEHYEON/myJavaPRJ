fbf004907be70d63644bcc7e066a86c3d8c6b62c
myJavaPRJ <ijh0547@naver.com>
2024-04-19 10:25:53 +0900

    중간고사 대비 별 찍기

--- src/main/java/ch_ex/Code_Quiz_03_19.java
+++ src/main/java/ch_ex/Code_Quiz_03_19.java
@@ -2,8 +2,8 @@ package ch_ex;
 
 public class Code_Quiz_03_19 {
     public static void main(String[] args) {
-        for(int i = 1; i <=5; i++) {
-            for (int j = 1; j <= 5 - i; j++){
+        for(int i = 1; i <=4; i++) {
+            for (int j = 1; j <= 4 - i; j++){
                 System.out.print(" ");
             }
             for (int h = 1; h<= i * 2 - 1; h++) {
