1e2a17495143160ee3fdf61f0fe29d8d26b37d66
myJavaPRJ <ijh0547@naver.com>
2024-04-19 10:26:08 +0900

    백준 문제풀이

---
+++ src/main/java/Bakejoon/Back_10926.java
@@ -0,0 +1,15 @@
+package Bakejoon;
+
+import java.util.Scanner;
+
+public class Back_10926 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String id;
+        id = s.nextLine();
+
+        System.out.println(id+"??!");
+
+    }
+}
---
+++ src/main/java/Bakejoon/Back_18108.java
@@ -0,0 +1,16 @@
+package Bakejoon;
+
+import java.util.Scanner;
+
+// 불기연도가 주어질때 서기 연도로 바꿔주는 프로그램
+
+public class Back_18108 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int y;
+        y = s.nextInt();
+
+        System.out.println(y-543);
+    }
+}
