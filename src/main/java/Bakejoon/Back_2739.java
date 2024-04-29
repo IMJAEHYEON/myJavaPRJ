84867502f77cd8518a37656988de079a99b5714c
myJavaPRJ <ijh0547@naver.com>
2024-04-29 13:56:47 +0900

    Scanner 함수와 for를 이용해 구구단

---
+++ src/main/java/Bakejoon/Back_2739.java
@@ -0,0 +1,18 @@
+package Bakejoon;
+
+import java.util.Scanner;
+
+public class Back_2739 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int N = s.nextInt();
+
+        for(int i = 1; i <= 9; i++){
+
+                System.out.println(N + "x" + i + "=" + (N*i));
+            }
+        }
+
+
+    }
