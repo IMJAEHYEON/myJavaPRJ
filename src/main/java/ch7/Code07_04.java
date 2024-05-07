4e862469ac3bc246f23aa04d52b586fe02daee3a
myJavaPRJ <ijh0547@naver.com>
2024-05-07 14:11:22 +0900

    거북이 객체 생성 1

---
+++ src/main/java/ch7/Code07_04.java
@@ -0,0 +1,11 @@
+package ch7;
+
+import Turtle.Turtle;
+
+public class Code07_04 {
+    public static void main(String[] args) {
+        Turtle trutle1 = new Turtle();
+        Turtle trutle2 = new Turtle();
+        Turtle trutle3 = new Turtle();
+    }
+}
