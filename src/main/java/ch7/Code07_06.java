5c516c7afbbe0378223adeed2b29ea875810cb6e
myJavaPRJ <ijh0547@naver.com>
2024-05-07 14:15:03 +0900

    토끼 클래스 생성

--- src/main/java/ch7/Code07_06.java
+++ src/main/java/ch7/Code07_06.java
@@ -8,11 +8,17 @@ class Rabbit { // 실제 코드에서는 권장하지않는 형식
     void setPosition(int x, int y){
         xPos = x;
         yPos = y;
+
+        System.out.println("xPos : " + xPos + " / yPos : " + yPos);
     }
 }
 
 public class Code07_06 {
     public static void main(String[] args) {
+        Rabbit rb1 = new Rabbit();
+        rb1.setPosition(100,100);
 
+        Rabbit rb2 = new Rabbit();
+        rb2.setPosition(200,200);
     }
 }
