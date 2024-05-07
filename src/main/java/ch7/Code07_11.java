fc67d1a9c8f1c7a741655a6bcbbbfb95b7c2ae16
myJavaPRJ <ijh0547@naver.com>
2024-05-07 15:16:30 +0900

    매개변수가 있는 생성자

--- src/main/java/ch7/Code07_11.java
+++ src/main/java/ch7/Code07_11.java
@@ -1,11 +1,12 @@
 package ch7;
+
 class Rabbit6 {
     String shape; // 토끼의 모양
     int xPos; // X 위치
     int yPos; // Y 위치
 
-    Rabbit6() {
-        shape = "토끼";
+    Rabbit6(String value) {
+        this.shape = value;
     }
 
     void setPosition(int x, int y) {
@@ -18,9 +19,12 @@ class Rabbit6 {
 
 public class Code07_11 {
     public static void main(String[] args) {
-        Rabbit rabbit1 = new Rabbit();
-        Rabbit rabbit2 = new Rabbit();
-        Rabbit rabbit3 = new Rabbit();
+        String rabbitcolor = "파란색";
+        Rabbit6 rabbit4 = new Rabbit6(rabbitcolor);
+        Rabbit6 rabbit1 = new Rabbit6("원");
+        Rabbit6 rabbit2 = new Rabbit6("삼각형");
+        Rabbit6 rabbit3 = new Rabbit6("토끼");
+        
 
         System.out.printf("토끼 객체의 기본 모양은 [%s]입니다.",rabbit1.shape);
         System.out.printf("토끼 객체의 기본 모양은 [%s]입니다.",rabbit2.shape);
