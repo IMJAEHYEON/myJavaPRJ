fbfd23e484a5dac9c38b057149b8223a79faddd7
myJavaPRJ <ijh0547@naver.com>
2024-05-07 14:12:05 +0900

    토끼 클래스 생성

---
+++ src/main/java/ch7/Code07_06.java
@@ -0,0 +1,18 @@
+package ch7;
+
+class Rabbit { // 실제 코드에서는 권장하지않는 형식
+    String shape; // 토끼의 모양
+    int xPos; // X 위치
+    int yPos; // Y 위치
+
+    void setPosition(int x, int y){
+        xPos = x;
+        yPos = y;
+    }
+}
+
+public class Code07_06 {
+    public static void main(String[] args) {
+
+    }
+}
