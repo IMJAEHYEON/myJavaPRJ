d4b580da07857cbbd68d5b787c33d4e556c1ac41
myJavaPRJ <ijh0547@naver.com>
2024-05-07 14:43:07 +0900

    속성에 값 대입

---
+++ src/main/java/ch7/Code07_08.java
@@ -0,0 +1,24 @@
+package ch7;
+class Rabbit3 { // 실제 코드에서는 권장하지않는 형식
+    String shape; // 토끼의 모양
+    int xPos; // X 위치
+    int yPos; // Y 위치
+
+    void setPosition(int x, int y){
+        this.xPos = x;
+        this.yPos = y;
+
+        System.out.println("xPos : " + this.xPos + " / yPos : " + this.yPos);
+    }
+}
+public class Code07_08 {
+    public static void main(String[] args) {
+        Rabbit rabbit1 = new Rabbit();
+        Rabbit rabbit2 = new Rabbit();
+        Rabbit rabbit3 = new Rabbit();
+
+        rabbit1.shape = "원";
+        rabbit2.shape = "삼각형";
+        rabbit3.shape = "토끼";
+    }
+}
