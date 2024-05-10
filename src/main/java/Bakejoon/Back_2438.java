48a53d5371c023cf8c5fb2fd099e11582f2b2591
myJavaPRJ <ijh0547@naver.com>
2024-05-10 11:15:28 +0900

    scanner 사용 해서 별찍기

---
+++ src/main/java/Bakejoon/Back_2438.java
@@ -0,0 +1,18 @@
+package Bakejoon;
+
+import java.util.Scanner;
+
+public class Back_2438 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
