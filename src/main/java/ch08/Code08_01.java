ae67d7a3eeb6f078825ee1ada7fbc2f22614c904
myJavaPRJ <ijh0547@naver.com>
2024-05-13 14:46:42 +0900

    클래스 변수

---
+++ src/main/java/ch08/Code08_01.java
@@ -0,0 +1,22 @@
+package ch08;
+class Rabbit{
+    String shape;
+    int xPos;
+    int yPos;
+    static int count;
+
+    Rabbit(){
+        count++; //new 함수로 메모리 올라갈 때마다 실행
+    }
+}
+public class Code08_01 {
+    public static void main(String[] args) {
+        System.out.println("객체 생성 전의 총 토끼의 수 ==> " + Rabbit.count);
+
+        Rabbit rabbit1 = new Rabbit();
+        System.out.println("토끼 객체1 생성 후의 총 토끼의 수 ==> " + Rabbit.count);
+
+        Rabbit rabbit2 = new Rabbit();
+        System.out.println("토끼 객체2 생성 후의 총 토끼의 수 ==> " + Rabbit.count);
+    }
+}
