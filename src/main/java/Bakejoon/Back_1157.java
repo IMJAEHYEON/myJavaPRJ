36e88954d3b101bfc191b608dfdf6d3a816f8838
myJavaPRJ <ijh0547@naver.com>
2024-06-14 12:01:12 +0900

    단어 공부 (가장 많이 사용된 단어 찾기)

---
+++ src/main/java/Bakejoon/Back_1157.java
@@ -0,0 +1,40 @@
+package Bakejoon;
+
+import java.util.Scanner;
+
+public class Back_1157 {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        String[] inp = s.next().toUpperCase().split("");
+        int[] count = new int[inp.length];
+        int max = 0;
+        int tmp = 0;
+        String result = "";
+
+        for (int k = 0; k < inp.length; k++) {
+            for (int j = 0; j < inp.length; j++) {
+                if (inp[k].equals(inp[j])) {
+                    count[j]++;
+                    break;
+                }
+            }
+        }
+
+        for (int l : count) {
+            max = Math.max(max, l);
+        }
+
+        for (int m = 0; m < count.length; m++) {
+            if (count[m] == max) {
+                result = inp[m];
+                tmp++;
+            } else if (tmp >= 2) {
+                result = "?";
+                break;
+            }
+        }
+
+        System.out.print(result);
+        s.close();
+    }
+}
